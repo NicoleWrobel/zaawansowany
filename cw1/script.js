ed90e2964e2075945e25d4f1a5a3d670db2a7e91
NicoleWrobel <59199173+Baszittaa@users.noreply.github.com>
2022-06-28 19:58:02 +0200

    pokaz slajdow

--- cw1/script.js
+++ cw1/script.js
@@ -1,14 +1,52 @@
-// Stwórz tablicę obiektów, z których każdy obiekt będzie zawierał tytuł i źródło zdjęcia
-// Stwórz funkcję, która będzie pobierała w argumencie numer slajdu (0 - 2) i na jego podstawie będzie wyświetlała wartości z tablicy (z pkt 1) w dokumencie html’a (będzie wyświetlała zdjęcie i tytuł)
-// Stwórz zmienną, która będzie zmieniała numery slajdów (np let currentSlide ...)
-// Stwórz kolejną funkcję, która będzie pobierać argument left lub right i dodaj do niej warunek, który w zależności od argumentu będzie zwiększał lub zmniejszał zmienną currentSlide, a następnie wykonywała [funkcja] funkcję stworzoną w pkt 2 ze zaktualizowanym argumentem currentSlide. 
-// Pobierz html’owe strzałki z dokumentu i dodaj do nich wydarzenia wywołujące funkcję z pkt 4 z odpowiednim argumentem (left lub right).
 
-
-const views =[
+let slideNr =0
+const data = [
     {
-        "src": "1.jpg",
-        "name": "Pierwszy"
+      title : "Pink lake",
+      src: "1.webp",
+    },
+    {
+      title : "Single Island",
+      src: "2.webp",
+    },
+    {
+      title : "Forest lake",
+      src: "3.webp",
+    },
+  ]
+  
+  function changeHTMLinfo( slideNr ){
+    const title = data[slideNr].title
+    const titleDOM = document.querySelector("h2")
+    titleDOM.innerHTML = title
+    const image = data[slideNr].src
+    const imageDOM = document.querySelector("img")
+    imageDOM.src = "img/"+image
+}
 
-    }
-]
+// changeHTMLInfo(1)
+
+const left = document.querySelector('.lefftArr')
+left.onclick = function(){
+    console.log(slideNr)
+    if (slideNr > 0)
+{
+slideNr = slideNr-1;
+}
+else slideNr = 2;
+changeHTMLinfo(slideNr);
+console.log(slideNr)
+}
+
+
+const right = document.querySelector('.rightArr')
+right.onclick = function(){
+    console.log(slideNr)  
+if (slideNr < 2){
+    slideNr = slideNr+1;
+}
+else slideNr = 0;
+changeHTMLinfo(slideNr);
+console.log(slideNr)
+}
+
